06d589ad7ef06f4e4182583af06ae1bf59b424b9
kho903 <gmldnr2222@naver.com>
2020-01-31 20:08:35 +0900

    Express Core: Middlewares

--- index.js
+++ index.js
@@ -13,8 +13,15 @@ const handleHome = (req, res) => res.send('Hello from home');
 const handleProfile = (req, res) => res.send("You are on my profile");
 
 
+const betweenHome = (req, res, next) =>{
+    console.log('Between');
+    next();
+}
+
+app.use(betweenHome);
+
 app.get("/", handleHome);
 
-app.get("/profile", handleProfile)
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
