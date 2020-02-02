e3f0dfcaa16547bce062111ca34f9a120c8e83da
kho903 <gmldnr2222@naver.com>
2020-02-02 19:46:19 +0900

    More Controlers

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,4 +1,5 @@
-import {videos} from "../db"
+import { videos } from "../db"
+import routes from "../routes"
 
 export const home = (req, res) => {
     
@@ -13,10 +14,26 @@ export const search = (req, res) =>{
     res.render("search", { pageTitle: "Search", searchingBy, videos });
 }
 
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) => 
+    res.render("upload", { pageTitle: "Upload" });
 
-export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const postUpload = (req, res) => {
+    const{
+        body:{
+            file,
+            title,
+            description,
+        }
+    } = req;
+    // To Do: Upload and save video
+    res.redirect(routes.videoDetail(324393));
+};
 
-export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+export const videoDetail = (req, res) => 
+    res.render("videoDetail", { pageTitle: "Video Detail" });
 
-export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const editVideo = (req, res) => 
+    res.render("editVideo", { pageTitle: "Edit Video" });
+
+export const deleteVideo = (req, res) => 
+    res.render("deleteVideo", { pageTitle: "Delete Video" });
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
 import routes from "../routes";
-import { videos, upload, videoDetail, editVideo, deleteVideo } from "../controllers/videoControllers";
+import { getUpload, videoDetail, editVideo, deleteVideo, postUpload } from "../controllers/videoControllers";
 
 export const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
