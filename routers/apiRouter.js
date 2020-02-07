eeb88217d66628310c30ce400fba3fc65c7703b9
kho903 <gmldnr2222@naver.com>
2020-02-07 20:44:22 +0900

    API Registering a View pt.1

---
+++ routers/apiRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+import routes from "../routes";
+import { postRegisterView } from "../controllers/videoControllers";
+
+const apiRouter = express.Router();
+
+apiRouter.get(routes.registerView, postRegisterView);
+
+export default apiRouter;
