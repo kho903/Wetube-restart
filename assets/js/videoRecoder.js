9ba434f1a8bd239ee7a823c68dc2c24f519c7b20
kho903 <gmldnr2222@naver.com>
2020-02-07 18:30:08 +0900

    Getting User Media

---
+++ assets/js/videoRecoder.js
@@ -0,0 +1,26 @@
+const recorderContainer = document.getElementById("jsRecordContainer");
+const recordBtn = document.getElementById("jsRecordBtn");
+const videoPreview = document.getElementById("jsVideoPreview");
+
+const startRecording = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: { width: 720, height: 480 }
+    });
+    videoPreview.srcObject = stream;
+    videoPreview.play();
+    videoPreview.muted = true;
+  } catch (error) {
+    recordBtn.innerHTML = "☹ Can't record";
+    recordBtn.removeEventListener("click", startRecording);
+  }
+};
+
+function init() {
+  recordBtn.addEventListener("click", startRecording);
+}
+
+if (recorderContainer) {
+  init();
+}
