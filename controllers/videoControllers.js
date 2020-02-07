f61115c717944848de0a75787b21e54e46cc50d5
kho903 <gmldnr2222@naver.com>
2020-02-07 21:31:34 +0900

    API Adding a Comment pt.1

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,6 +1,6 @@
 import routes from "../routes";
 import Video from "../models/Video";
-import e from "express";
+import Comment from "../models/Comment";
 
 // Home
 
@@ -61,7 +61,9 @@ export const videoDetail = async (req, res) => {
     params: { id }
   } = req;
   try {
-    const video = await Video.findById(id).populate("creator");
+    const video = await Video.findById(id)
+      .populate("creator")
+      .populate("comments");
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     res.redirect(routes.home);
@@ -131,6 +133,29 @@ export const postRegisterView = async (req, res) => {
     res.status(200);
   } catch (error) {
     res.statusCode(400);
+  } finally {
+    res.end();
+  }
+};
+
+// Add Comment
+
+export const postAddComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { comment },
+    user
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    const newComment = await Comment.create({
+      text: comment,
+      creator: user.id
+    });
+    video.comments.push(newComment.id);
+    video.save();
+  } catch (error) {
+    res.status(400);
   } finally {
     res.end();
   }
